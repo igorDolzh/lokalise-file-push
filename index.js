e7f9b8cf424470e69dc0940c0df922bb3c47fb86
Igor Dolzhenkov <igor.techweb@gmail.com>
2019-12-24 18:54:35 +0300

    First action

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const fs = require("fs");
+const core = require("./core");
+const ghCore = require("@actions/core");
+const { LokaliseApi } = require("@lokalise/node-api");
+
+const apiKey = ghCore.getInput("api-token");
+const projectId = ghCore.getInput("project-id");
+const directory = ghCore.getInput("directory");
+const format = ghCore.getInput("format");
+const platform = ghCore.getInput("platform");
+const filename = ghCore.getInput("filename");
+
+core(
+  {
+    apiKey,
+    projectId,
+    directory: path.join(process.env.GITHUB_WORKSPACE, directory),
+    format,
+    platform,
+    filename
+  },
+  {
+    LokaliseApi,
+    fs
+  }
+)
+  .then(() => console.log("Finished"))
+  .catch(error => ghCore.setFailed(error ? error.message : "Unknown error"));
