66ff45a21ca563ddd1b3701ca81a2962d9fc7fa8
Igor Dolzhenkov <igor.techweb@gmail.com>
2020-10-15 13:57:13 +0300

    Test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ const fs = require("fs");
 
 const LANG_ISO_PLACEHOLDER = "%LANG_ISO%";
 
+const getContext = (text) => {
+  const foundText = text.match(
+    /<!--- LOKALIZE CONTEXT FOR TRANSLATORS -->(\n| )*((.|\n)*)(\n| )*<!--- LOKALIZE CONTEXT FOR TRANSLATORS -->/
+  );
+  return foundText ? foundText[2] : "";
+};
+
+const getClubhouseLink = (text) => {
+  const clubhouseLink = text.match(/Clubhouse Story: (.*)/);
+  return clubhouseLink ? clubhouseLink[0] : "";
+};
+
+const getAllImages = (text) => {
+  const listOfImages = text.match(
+    /(http|ftp|https):\/\/(user-images.githubusercontent.com)([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/g
+  );
+  return listOfImages || [];
+};
+
+const getTaskDescription = (description) => {
+  if (description) {
+    const contextText = getContext(description).trim();
+    if (!contextText) {
+      return description;
+    }
+    const clubhouseLink = getClubhouseLink(description);
+    const allImages = getAllImages(description);
+
+    return `${clubhouseLink ? `${clubhouseLink}\n` : ""}${contextText}${
+      allImages && allImages.length > 0
+        ? `\n${allImages.map((path) => `screenshot: ${path}`).join("\n")}`
+        : ""
+    }`;
+  }
+  return "";
+};
+
 async function getLanguageISOCodes(lokalise, projectId) {
   const languages = await lokalise.languages.list({
     project_id: projectId,
