e887b38cec1eff6e1d7ca93ffa57e1ce918b0880
Igor Dolzhenkov <igor.techweb@gmail.com>
2020-10-15 14:19:41 +0300

    Save

--- helpers.js
+++ helpers.js
@@ -1,31 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const getContext = (text) => {
-    console.log("getContext");
-    console.log(text);
-    const foundText = text.match(/<!--- LOKALIZE CONTEXT FOR TRANSLATORS -->(\n| )*((.|\n)*)(\n| )*<!--- LOKALIZE CONTEXT FOR TRANSLATORS -->/);
-    console.log(foundText);
-    return foundText ? foundText[2] : "";
+  console.log("getContext");
+  console.log(text);
+  const foundText = text.match(
+    /<!--- LOKALIZE CONTEXT FOR TRANSLATORS -->((.|\n)*)<!--- LOKALIZE CONTEXT FOR TRANSLATORS -->/
+  );
+  console.log(foundText);
+  return foundText ? foundText[1] : "";
 };
 const getClubhouseLink = (text) => {
-    const clubhouseLink = text.match(/Clubhouse Story: (.*)/);
-    return clubhouseLink ? clubhouseLink[0] : "";
+  const clubhouseLink = text.match(/Clubhouse Story: (.*)/);
+  return clubhouseLink ? clubhouseLink[0] : "";
 };
 const getAllImages = (text) => {
-    const listOfImages = text.match(/(http|ftp|https):\/\/(user-images.githubusercontent.com)([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/g);
-    return listOfImages || [];
+  const listOfImages = text.match(
+    /(http|ftp|https):\/\/(user-images.githubusercontent.com)([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/g
+  );
+  return listOfImages || [];
 };
 exports.getTaskDescription = (description) => {
-    if (description) {
-        const contextText = getContext(description).trim();
-        if (!contextText) {
-            return description;
-        }
-        const clubhouseLink = getClubhouseLink(description);
-        const allImages = getAllImages(description);
-        return `${clubhouseLink ? `${clubhouseLink}\n` : ""}${contextText}${allImages && allImages.length > 0
-            ? `\n${allImages.map((path) => `screenshot: ${path}`).join("\n")}`
-            : ""}`;
+  if (description) {
+    const contextText = getContext(description).trim();
+    if (!contextText) {
+      return description;
     }
-    return "";
+    const clubhouseLink = getClubhouseLink(description);
+    const allImages = getAllImages(description);
+    return `${clubhouseLink ? `${clubhouseLink}\n` : ""}${contextText}${
+      allImages && allImages.length > 0
+        ? `\n${allImages.map((path) => `screenshot: ${path}`).join("\n")}`
+        : ""
+    }`;
+  }
+  return "";
 };
